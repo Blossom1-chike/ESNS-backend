61a66051cc57fc4bc5a8596c487f4d2debfde7b7
Chikezie Blossom <chinanuchikezie515@gmail.com>
2023-04-17 02:35:39 +0100

    cors changes

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-require('dotenv').config(); //Loads all environment variables to be used in the project from process.env file
-const express = require('express');
-const cors = require('cors');
-const { connectToDB } = require('./database/config.js');
+require("dotenv").config(); //Loads all environment variables to be used in the project from process.env file
+const express = require("express");
+const cors = require("cors");
+const { connectToDB } = require("./database/config.js");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const http = require("http");
 
 const PORT = process.env.SERVER_PORT || process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: [
-        "http://localhost:3000",
-        "https://esns-frontend-6b1l2ytol-blossom1-chike.vercel.app/", 
-        "https://esns-backend-production-5467.up.railway.app/"
-    ],
-    credentials: true,  //access-control-allow-credentials:true
-    optionSuccessStatus: 200
+  origin: [
+    "http://localhost:3000",
+    "https://esns-frontend-6b1l2ytol-blossom1-chike.vercel.app/",
+    "https://esns-backend-production-5467.up.railway.app/",
+  ],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 //To allow CORS
 app.use(cors(corsOptions));
@@ -24,24 +33,24 @@ app.use(cors(corsOptions));
 //To allow json requests and to decode requests from forms
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//To allow cookies 
+//To allow cookies
 app.use(cookieParser());
 
 //Server creation and database setup
 const server = http.createServer(app);
 
 connectToDB()
-.then(() => {
+  .then(() => {
     //Start listening to the server at port assigned
     server.listen(PORT, () => {
-        console.log('Server listening at port: ', PORT);
-    })
-})
-.catch(() => {
-    console.log('Error occured while connecting to database: ')
-});
+      console.log("Server listening at port: ", PORT);
+    });
+  })
+  .catch(() => {
+    console.log("Error occured while connecting to database: ");
+  });
 
 //Routes
 app.use("/api", require("./controllers/user"));
@@ -50,14 +59,11 @@ app.use("/api", require("./controllers/history"));
 app.use("/api/admin", require("./controllers/admin")); //Admin route
 
 //When a route is non-existent
-app.use('*', (req, res) => {
-    res.status(404).send({error: 'Route does exist'});
-})
+app.use("*", (req, res) => {
+  res.status(404).send({ error: "Route does exist" });
+});
 //Any error occured in starting the server
 server.on("error", (err) => {
-    console.log("Error occured while starting server: ", err);
-    process.exit(1);
-})
-
-
-
+  console.log("Error occured while starting server: ", err);
+  process.exit(1);
+});
