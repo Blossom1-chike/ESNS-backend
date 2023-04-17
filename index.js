ee80392ced011c8f935c9c08988c0e4054de41a6
Chikezie Blossom <chinanuchikezie515@gmail.com>
2023-04-17 02:20:48 +0100

    cors stuff

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const PORT = process.env.SERVER_PORT || process.env.PORT || 4000;
 
 const corsOptions = {
     origin: [
-        // "http://localhost:3000",
+        "http://localhost:3000",
+        "https://esns-frontend-6b1l2ytol-blossom1-chike.vercel.app/", 
         "https://esns-backend-production-5467.up.railway.app/"
     ],
     credentials: true,  //access-control-allow-credentials:true
